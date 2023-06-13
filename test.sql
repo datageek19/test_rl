32aac0cb6b0cd518834713438050d1fd213bc680
datageek19 <45903389+datageek19@users.noreply.github.com>
2023-06-13 13:38:58 -0500

    Update test.sql

--- test.sql
+++ test.sql
@@ -4,4 +4,16 @@ WHERE ((std_servicedate >= '2022-08-01') AND (sdr_received_source LIKE 'PHIL%') 
 GROUP BY sdr_received_source, cvx_code, procedurecode;
 
 
+-- ====================
+SELECT date_format(date_sub(date, cast(date_format(date, 'u') as int) - 1), 'yyyy-MM-dd') AS week_start_date, COUNT(lab_id) AS lab_count
+FROM your_table_name
+GROUP BY date_sub(date, cast(date_format(date, 'u') as int) - 1)
+ORDER BY week_start_date;
+
+-- Total lab counts by month
+SELECT date_format(date_sub(date, cast(date_format(date, 'd') as int) - 1), 'yyyy-MM-dd') AS month_start_date, COUNT(lab_id) AS lab_count
+FROM your_table_name
+GROUP BY date_sub(date, cast(date_format(date, 'd') as int) - 1)
+ORDER BY month_start_date;
+
 
