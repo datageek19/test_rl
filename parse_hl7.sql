a10980754cdd13a17595a45e780130eeb70736d0
datageek19 <45903389+datageek19@users.noreply.github.com>
2024-03-28 09:58:30 -0500

    Update parse_hl7.sql

--- parse_hl7.sql
+++ parse_hl7.sql
@@ -21,3 +21,25 @@ SELECT
   SPLIT_PART(SPLIT_PART(REGEXP_SUBSTR(message, 'PV1\\|([^|]*\\|){1}([^|]*)'), '|', 2), '^', 1) AS PV1_2_1,
   SPLIT_PART(SPLIT_PART(REGEXP_SUBSTR(message, 'PV1\\|([^|]*\\|){2}([^|]*)'), '|', 3), '^', 1) AS PV1_3_1
 FROM hl7
+
+
+-- solution 2
+
+SELECT
+    -- Parse out string at 3rd delimiter after MSH key word
+    SPLIT_PART(SPLIT_PART(REGEXP_SUBSTR(message, 'MSH\\|([^|]*\\|){2}([^|]*)'), '|', 3), '^', 1) AS MSH_3
+
+    -- Parse out string at 20th delimiter after MSH key word then parse out string at 1st caret sign after @MSH.3
+    SPLIT_PART(SPLIT_PART(REGEXP_SUBSTR(message, 'MSH\\|([^|]*\\|){19}([^|]*)'), '|', 20), '^', 1) AS MSH_20_1,
+    -- Parse out string at 20th delimiter after MSH key word then parse out string at 2nd caret sign after @MSH.4
+    SPLIT_PART(SPLIT_PART(REGEXP_SUBSTR(message, 'MSH\\|([^|]*\\|){19}([^|]*)'), '|', 20), '^', 2) AS MSH_20_2
+
+    -- Parse out string at 1st, 2nd, 3rd, 4th, 6th, 37th, 39th delimiter after PV1 key word
+    SPLIT_PART(REGEXP_SUBSTR(message, 'PV1\\|([^|]*\\|){0}([^|]*)'), '|', 1) AS PV1_1,
+    SPLIT_PART(REGEXP_SUBSTR(message, 'PV1\\|([^|]*\\|){1}([^|]*)'), '|', 2) AS PV1_2,
+    SPLIT_PART(REGEXP_SUBSTR(message, 'PV1\\|([^|]*\\|){2}([^|]*)'), '|', 3) AS PV1_3,
+    SPLIT_PART(REGEXP_SUBSTR(message, 'PV1\\|([^|]*\\|){3}([^|]*)'), '|', 4) AS PV1_4,
+    SPLIT_PART(REGEXP_SUBSTR(message, 'PV1\\|([^|]*\\|){5}([^|]*)'), '|', 6) AS PV1_6,
+    SPLIT_PART(REGEXP_SUBSTR(message, 'PV1\\|([^|]*\\|){36}([^|]*)'), '|', 37) AS PV1_37,
+    SPLIT_PART(REGEXP_SUBSTR(message, 'PV1\\|([^|]*\\|){38}([^|]*)'), '|', 39) AS PV1_39
+
