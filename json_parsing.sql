13c3753412ca059008addc94d7f0da091ce3e3aa
datageek19 <45903389+datageek19@users.noreply.github.com>
2024-09-12 10:52:16 -0500

    Create json_parsing.sql

---
+++ json_parsing.sql
@@ -0,0 +1,28 @@
+select
+  KVALUE:rawPayload::string,
+  substring(tracking_id,1,4) as submitter,
+  split_part(msh21_msh3.value,'^',2) as msh3_code,
+  split_part(msh21_msh3.value,'^',3) as msh3_oid,
+  split_part(msh21_msh4.value,'^',2) as msh4_code,
+  split_part(msh21_msh4.value,'^',3) as msh4_oid,
+  split_part(split_part(enc.value,'|',4),'^',1) as pv1_3_1,
+  split_part(split_part(enc.value,'|',4),'^',2) as pv1_3_2,
+  split_part(split_part(enc.value,'|',4),'^',3) as pv1_3_3,
+  split_part(split_part(split_part(enc.value,'|',4),'^',4),'&',1) as pv1_3_4_1,
+  split_part(split_part(split_part(enc.value,'|',4),'^',4),'&',2) as pv1_3_4_2,
+  split_part(enc.value,'|',40) as pv1_39
+from source_raw_file_tables SRC 
+  join lateral flatten(split(KVALUE:rawPayload,'\r')) pid
+  join lateral flatten(split(KVALUE:rawPayload,'\r')) enc
+  --join lateral flatten(input => split(KVALUE:rawPayload,'\r')) pv2
+  join lateral flatten(split(KVALUE:rawPayload,'\r')) msh
+  join lateral flatten (split(split_part(msh.value,'|',21),'~')) msh21_msh3
+  join lateral flatten (split(split_part(msh.value,'|',21),'~')) msh21_msh4
+  where 
+  submitter='9060'
+  and msh.index = 0 -- this is MSH
+  and split_part(pid.value::string,'|',1) = 'PID'
+  and split_part(enc.value::string,'|',1) = 'PV1'
+  -- and split_part(pv2.value::string,'|',1) = 'PV2'
+  and substring(msh21_msh3.value::string,1,6) = '@MSH.3'
+  and substring(msh21_msh4.value::string,1,6) = '@MSH.4'
